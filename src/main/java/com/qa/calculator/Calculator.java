8531e5167e9a278386f210a465ae81402e870daa
Charlie Reddin <creddin788@gmail.com>
2020-02-10 14:27:15 +0000

    Fucking fixed it

--- src/main/java/com/qa/calculator/Calculator.java
+++ src/main/java/com/qa/calculator/Calculator.java
@@ -2,9 +2,13 @@ package com.qa.calculator;
 
 public class Calculator {
 
-	public Calculator() {
+	public int Addition() {
 		
+		int a = 1;
+		int b = 2;
+		int c = a+b;
+		return c;
 		
 	}
-	
+
 }
--- src/main/java/com/qa/calculator/Runner.java
+++ src/main/java/com/qa/calculator/Runner.java
@@ -5,6 +5,9 @@ public class Runner {
 	public static void main(String[] args) {
 		
 		Calculator calculator = new Calculator();
+		
+		calculator.Addition();
+		
 
 	}
 
