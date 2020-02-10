a5d7d67a36896984a8a0b078494fe22fc4b38a8e
Johannes <Johannes.Bull@academytrainee.com>
2020-02-10 14:46:40 +0000

    Fixed method name

--- src/main/java/com/qa/calculator/Calculator.java
+++ src/main/java/com/qa/calculator/Calculator.java
@@ -7,7 +7,7 @@ public class Calculator {
 		
 	}
 	
-	public double Divide(double num1, double num2) {
+	public double divide(double num1, double num2) {
 		if (num2 != 0) {
 			return num1/num2;
 		}else {
